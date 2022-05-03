1bcb20af439d263265a25dc2d90aad2d6a46042c
Lizobuy <102720671+Lizobuy@users.noreply.github.com>
2022-05-03 15:18:27 +0200

    Add project files.

---
+++ DemoXamarin/DemoXamarin/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _numberClicked;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+
+        {
+            _numberClicked++;
+
+            ClickCountLabel.Text = _numberClicked.ToString();
+        }
+    }
+}
